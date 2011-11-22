b5f02d8aaa3a6386750706507b9ea155f2d69557
Damoun <damoun60@gmail.com>
2011-11-22 21:03:53 +0100

    local pipeline

---
+++ sources/pipeline/Local.js
@@ -0,0 +1,54 @@
+//!requires:Pipeline
+//!provides:Pipeline.Local
+//
+//!requires:JS.Class
+//!requires:Pipeline.Base
+
+/*
+ * @class
+ */
+
+Pipeline.Local = new JS.Class('Pipeline.Local', Pipeline.Base, {
+	
+	/*
+	 * @constructor
+	 */
+	
+	initialize: function ( ) {
+		
+		this.callSuper( );
+		
+	},
+
+	/*
+	 * @param {LocalPipeline} client Pipeline client avec lequel communiquer
+	 */
+
+    connect: function ( multiplexer ) {
+	    
+	    this.multiplexer = multiplexer;
+
+    },
+	
+	/*
+	 * @param {String} command La command a envoyer
+	 * @param {Object} message Data a envoyer
+	 */
+	
+	send: function ( command, message ) {
+		
+		if ( typeof (this.referent) !== 'undefined' ) {
+			
+			this.multiplexer.trigger( command, message );
+			
+		} else {
+			
+			throw "LocalPipeline Class: sendCommand method: referent doesn't set";
+			
+		}
+		
+	},
+
+
+	
+});
---
+++ sources/pipeline/multiplexer/Local.js
@@ -0,0 +1,23 @@
+//!requires:Pipeline.Multiplexer
+//!provides:Pipeline.Multiplexer.Local
+//
+//!requires:JS.Class
+//!requires:Pipeline.Multiplexer.Base
+
+/*
+ * @class
+ */
+
+Pipeline.Local = new JS.Class('Pipeline.Multiplexer.Local', Pipeline.Multiplexer.Base, {
+	
+	/*
+	 * @constructor
+	 */
+	
+	initialize: function ( ) {
+		
+		this.callSuper( );
+		
+	}
+
+});
