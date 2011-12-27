00e6cc6149f3db334fe17394e09766520874fad6
Maël Nison <nison.mael@gmail.com>
2011-12-27 02:50:56 +0100

    Fixed compilation issues

---
+++ sources/clients/core/protocol/events/player/Join.js
@@ -0,0 +1,9 @@
+//!requires:Client.Core.Protocol.Event.Player
+//!provides:Client.Core.Protocol.Event.Player.Join
+//!requires:Client.Core.Protocol.Event.Player.Base
+// 
+//!requires:JS.Class
+
+Client.Core.Protocol.Event.Player.Join = new JS.Class('Client.Core.Protocol.Event.Player.Join', Client.Core.Protocol.Event.Player.Base, {
+	
+});
---
+++ sources/clients/core/protocol/events/player/Part.js
@@ -0,0 +1,9 @@
+//!requires:Client.Core.Protocol.Event.Player
+//!provides:Client.Core.Protocol.Event.Player.Part
+//!requires:Client.Core.Protocol.Event.Player.Base
+// 
+//!requires:JS.Class
+
+Client.Core.Protocol.Event.Player.Part = new JS.Class('Client.Core.Protocol.Event.Player.Part', Client.Core.Protocol.Event.Player.Base, {
+	
+});
