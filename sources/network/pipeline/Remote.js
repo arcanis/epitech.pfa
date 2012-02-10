dfe0d1fc85c8f7ada12d42fad5580dd44f5ef433
Mael Nison <nison.mael@gmail.com>
2012-02-10 10:35:12 +0100

    Move uuid generation in the Pipeline base class

--- sources/network/pipeline/Remote.js
+++ sources/network/pipeline/Remote.js
@@ -6,17 +6,9 @@
 //!uses:Network.Event.Disconnection
 //!uses:Network.Event.Message
 
-//[1] http://stackoverflow.com/questions/105034/how-to-create-a-guid-uuid-in-javascript
-
 Network.Pipeline.Remote = new JS.Class( 'Network.Pipeline.Remote', Network.Pipeline, {
 	
 	initialize : function ( siostream ) {
-		
-		// Generate a guid [1]
-		this.id = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace( /[xy]/g, function ( c ) {
-			var r = Math.random( ) * 16 | 0, v = c == 'x' ? r : ( r & 0x3 | 0x8 );
-			return v.toString( 16 );
-		} );
 		
 		this.siostream = siostream;
 		
@@ -33,6 +25,8 @@ Network.Pipeline.Remote = new JS.Class( 'Network.Pipeline.Remote', Network.Pipel
 			this.notifyObservers( disconnectionEvent );
 			
 		}.bind( this ) );
+		
+		this.callSuper( );
 		
 	},
 	
--- sources/network/pipeline/base.js
+++ sources/network/pipeline/base.js
@@ -6,6 +6,17 @@
 
 Network.Pipeline = new JS.Class( 'Network.Pipeline', {
 	
-	include : [ JS.Observable ]
+	include : [ JS.Observable ],
+	
+	initialize : function ( ) {
+		
+		// http://stackoverflow.com/questions/105034/how-to-create-a-guid-uuid-in-javascript
+		
+		this.uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace( /[xy]/g, function ( c ) {
+			var r = Math.random( ) * 16 | 0, v = c == 'x' ? r : ( r & 0x3 | 0x8 );
+			return v.toString( 16 );
+		} );
+		
+	}
 	
 } );
