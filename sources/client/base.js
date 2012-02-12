268953b20de6fe9785b4af6ec1aa772248b1a716
Mael Nison <nison.mael@gmail.com>
2012-02-12 21:51:37 +0100

    Moves client's .output property declaration

--- sources/client/base.js
+++ sources/client/base.js
@@ -2,11 +2,14 @@
 // 
 //!requires:JS.Class
 // 
+//!uses:Output.Emitter
 //!uses:Plugin
 
 global.Client = new JS.Class( 'Client', {
 	
 	initialize : function ( ) {
+		
+		this.output = new Output.Emitter( );
 		
 		Plugin.plug( this );
 		
