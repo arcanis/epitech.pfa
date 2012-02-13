52c60fba9d57406e733d4e1e1b5d2f04b17e3673
Mael Nison <nison.mael@gmail.com>
2012-02-13 06:02:35 +0100

    Sets camera construction parameters

--- sources/engine/Camera.js
+++ sources/engine/Camera.js
@@ -7,7 +7,7 @@ Engine.Camera = new JS.Class( 'Engine.Camera', {
 	
 	initialize : function ( ) {
 		
-		this.threeElement = new THREE.PerspectiveCamera( );
+		this.threeElement = new THREE.PerspectiveCamera( 60, 1, 0.1, 10000 );
 		
 	}
 	
